3c4c2863e9464306c0dae4364ad0793c9968685d
Chathurya-maddika <maddikachathurya@gmail.com>
2025-01-04 22:00:15 +0530

    Add files via upload

---
+++ Session_4.java
@@ -0,0 +1,23 @@
+public class Session_4 {
+    public static void main(String[] args) {
+        System.out.println(Byte.MIN_VALUE);
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println("MAX_VALUE+1:"+(Integer.MAX_VALUE));
+        System.out.println("MAX_VALUE*1:"+(Integer.MAX_VALUE));
+        System.out.println("Size of Integer: "+Integer.SIZE);
+        System.out.println("Size of Integer: "+(Integer.MIN_VALUE));
+        System.out.println("Size of Integer: "+(Integer.MAX_VALUE));
+
+        int x = 4;
+        int y = 8;
+        if(x > y){
+            System.out.println("x is greater");
+        }
+        else{
+            System.out.println("y is greater");
+        }
+    }
+    
+}
